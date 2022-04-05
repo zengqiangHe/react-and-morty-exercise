1abf1c3d3525b07c88f39f79904c7601dbdd1409
zengqiangHe <zengqiang.he@outlook.com>
2022-04-05 15:15:29 +0200

    add header image and content  without css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React and Morty</h1>
       </header>
+      <content>
+      <article className="cards">
+        <img
+          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+          alt="a pic of Morty Smith"
+        />
+        <h2>Morty Smith</h2>
+      </article>
+      </content>
+      <nav className="nav-container">
+      <ul className="nav-list-items">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+      </nav>
     </div>
   );
 }
