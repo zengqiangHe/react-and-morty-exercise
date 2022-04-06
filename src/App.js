8c246a3e43b10825a8e7df4f2d4e465f1c85f1cf
zengqiangHe <zengqiang.he@outlook.com>
2022-04-06 12:20:24 +0200

    add the API fetch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import "./App.css";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Cards from "./components/Cards";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [character, setCharacter] = useState([]);
+
+  const loadCharacter = () => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacter(data.results));
+  };
+
+  useEffect(() => {
+    loadCharacter();
+  }, []);
   return (
     <div className="App">
       <Header />
-      <Cards />
+      {character.map((character) => (
+            <Cards
+              key={character.id}
+              image={character.image}
+              name={character.name}
+            />
+          ))}
       <Navigation />
     </div>
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-const Cards = () => {
+const Cards = ({ image, name }) => {
   return (
     <CardsContainer>
       <CardsItems>
-        <img
-          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-          alt="a pic of Morty Smith"
-        />
-        <h2>Morty Smith</h2>
-      </CardsItems>
+     <img src={image} alt='a pic of character'/>
+     <h2>{name}</h2>
+     </CardsItems>
     </CardsContainer>
   );
 };
@@ -19,20 +16,17 @@ const CardsItems = styled.li`
   display: flex;
   flex-direction: column;
   list-style-type: none;
-  background-color: #7CD1B8;
-  width:70%;
-  margin-top:150px;
-  margin-right:20px;
-  padding:10px;
-  color:white;
-  border-radius:5%;
-  box-shadow:3px 4px 4px white;
-  
-
+  background-color: #7cd1b8;
+  width: 70%;
+  margin-top: 15px;
+  margin-right: 20px;
+  padding: 10px;
+  color: white;
+  border-radius: 5%;
+  box-shadow: 3px 4px 4px white;
 `;
 const CardsContainer = styled.ul`
-display:flex;
-justify-content:center;
-
+  display: flex;
+  justify-content: center;
 `;
 export default Cards;
